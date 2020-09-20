abb3c53ead4091f500cd4340639d0ddaf3dbb63a
Change-Only <756652729@qq.com>
2020-09-20 15:48:17 +0800

    6.2

--- 2.1.c
+++ 2.1.c
@@ -1,27 +1,29 @@
-//#include<stdio.h>
-//#include<math.h>
-//#pragma warning(disable:4996)
-//void main() {
-//	float a, b, c, s, area;
-//	printf("Please input a,b,c:");
-//	scanf("%f%f%f", &a, &b, &c);
-//	if (a + b > c && a + c > b && b + c > a) {
-//		s = (a + b + c)/2.0;
-//		area = sqrt(s * (s - a) * (s - b) * (s - c));
-//		printf("a=%8.2f,b=%8.2f,c=%8.2f", a, b, c);
-//		printf("Area=%8.2f", area);
-//	}
-//
-//}
 #include <stdio.h>
-#include <stdlib.h>
-#include<time.h>
 void main() {
-	int a[5], i;
-	srand((unsigned)time(NULL));
-	for (i = 0; i < 5; i++)
-		a[i] = rand() % 100;
-	for (i = 0; i < 5; i++)
-		printf("%5d", a[i]);
+	int i, x, j, a[20], t;
+	printf("请输入一个正整数：");
+	while (1)
+	{
+		scanf("%d", &x);
+		if (x < 0)
+			printf("输入的数据有误，请重新输入：");
+		else
+			break;
+	}
+	i = 0;
+	t = x;
+	while (x)
+	{
+		a[i] = x % 16;
+		x = x / 16;
+		i++;
+	}
+	for ( j = i-1; j >= 0; j--)
+	{
+		if (a[j] <= 9)
+			printf("%d", a[j]);
+		else if (a[j] <= 15 )
+			printf("%c", a[j] + 'A' - 10);
+	}
 	printf("\n");
 }
