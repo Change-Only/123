1b7841321149ae2cc22355889c08ef3def157f6a
Change-Only <756652729@qq.com>
2020-09-21 14:31:43 +0800

    yue se fu

--- 2.1.c
+++ 2.1.c
@@ -1,29 +1,26 @@
 #include <stdio.h>
-void main() {
-	int i, x, j, a[20], t;
-	printf("请输入一个正整数：");
-	while (1)
-	{
-		scanf("%d", &x);
-		if (x < 0)
-			printf("输入的数据有误，请重新输入：");
-		else
-			break;
+void  main() {
+	int a[100];
+	int i, j, m, n, k = 0;
+	printf("请输入人数n和任选一个正整数m：");
+	scanf("%d%d", &n, &m);
+	printf("输入n个人的密码：\n");
+	for (i = 0; i < n; i++) {
+		scanf("%d", &a[i]);
 	}
-	i = 0;
-	t = x;
-	while (x)
-	{
-		a[i] = x % 16;
-		x = x / 16;
-		i++;
+	printf("\n出列顺序为：");
+	for (i = 0; i < n; i++) {
+		j = 1;
+		while (j < m) {
+			while (a[k] == 0)
+				k = (k + 1) % n;
+			j++;
+			k = (k + 1) % n;
+		}
+		while (a[k]==0)
+			k = (k + 1) % n;
+		printf("%d", k + 1);
+		m = a[k];
+		a[k] = 0;
 	}
-	for ( j = i-1; j >= 0; j--)
-	{
-		if (a[j] <= 9)
-			printf("%d", a[j]);
-		else if (a[j] <= 15 )
-			printf("%c", a[j] + 'A' - 10);
-	}
-	printf("\n");
 }
